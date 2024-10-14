3cb6591c5eadffe6ad03aa691d1beeb5dc5aa1f4
Soham Dutta <duttasoham2222@gmail.com>
2024-10-14 18:58:22 +0530

    2nd commit

--- src/Components/Login/LoginComp.jsx
+++ src/Components/Login/LoginComp.jsx
@@ -55,6 +55,32 @@ function LoginComp() {
         </div>
       </div>
       <div className="w-1/2 relative overflow-x-hidden overflow-y-hidden">
+      <div className="flex flex-col">
+          <h2 className='font-semibold text-5xl'>Sign In</h2>
+          <form className="flex flex-col mt-8 gap-4 items-center relative">
+            <input type="email" placeholder='Email Address' className='w-[400px] bg-gray-100 rounded-lg p-2' />
+            <input type='password' placeholder='Password' className='w-[400px] bg-gray-100 rounded-lg p-2' />
+            <div className="flex justify-end w-full">
+              <a href='#' className='ml-120 text-blue-500'>Forget Password?</a>
+            </div>
+            <button type='submit' className='bg-blue-500 p-2 text-white rounded-lg w-[140px]'>Sign In</button>
+          </form>
+        </div>
+        <div className="flex flex-col items-center gap-4 mt-4">
+          <div className="flex items-center w-full">
+            <hr className='flex-grow border-gray border-t-2' />
+            <span className='mx-4 text-gray-600'>or continue with</span>
+            <hr className='flex-grow border-gray border-t-2' />
+          </div>
+          <div className="flex">
+            <button className='flex items-center border border-gray-600 rounded-md p-2 mr-4 gap-x-4'><FcGoogle /> Sign in with Google</button>
+            <button className='flex items-center border border-gray-600 rounded-md p-2 gap-x-4'><FaApple /> Sign in with Apple</button>  
+          </div>
+          <div className="">
+            <span className='text-gray-600'>Don't have an account?</span>
+            <a href='#' className='text-blue-700 font-semibold'>Register Now</a>
+          </div>
+        </div>
         <img src={decorate} alt='decoration' className='absolute right-[-300px] bottom-[-200px] w-[800px] h-auto object-cover' />
       </div>
     </div>

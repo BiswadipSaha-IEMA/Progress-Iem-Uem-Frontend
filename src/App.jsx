41fde42cd9199a7b27ee8a084e7a0f848809b1f2
Atrika10 <exyouknow15@gmail.com>
2024-10-15 10:37:04 +0530

    Final commit - After mergeing

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ChangePasswordComp from './Components/ChangePassword/ChangePasswordComp'
+
+function App() {
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default App

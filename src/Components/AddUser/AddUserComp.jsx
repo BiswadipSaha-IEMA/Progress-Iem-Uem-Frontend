6adefef32fd92f76361173015aef6e4db92028d9
BiswadipSaha-IEMA <biswadip.saha@iem.edu.in>
2024-10-24 10:47:29 +0530

    update ui

--- src/Components/AddUser/AddUserComp.jsx
+++ src/Components/AddUser/AddUserComp.jsx
@@ -1,44 +1,46 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { CiSquarePlus } from "react-icons/ci";
 import { CiSearch } from "react-icons/ci";
 import { IoIosCloseCircleOutline } from "react-icons/io";
 import MemberCard from '../MemberCard/MemberCard'
 import Sidebar from '../SideBar/Sidebar';
 import { Menu } from 'lucide-react';
+import gsap from 'gsap';
 
 const AddUserComp = () => {
-    const [sidebar,setSidebar]=useState(false)
-  return (
-    <div className="flex">
-        <Sidebar showProfile={sidebar} />
-        <div
-          className={`flex-grow rounded-lg mb-8 duration-300 ${
-            sidebar ? "lg:w-[calc(100% - 340px)] lg:ml-[320px]" : "lg:w-full lg:ml-0"
-          } bg-[url('/src/assets/image2.svg')]`}
-        >
-            <button className='bg-slate-200 p-2 rounded' onClick={()=>{setSidebar(!sidebar)}}><Menu /></button>
-            
-            <div className="flex justify-between items-center">
-                <div className="w-[83%] relative flex items-center">
-                    <button className="absolute text-3xl left-2"><CiSearch/></button>
-                    <input className='w-full rounded-lg p-2 pl-12 focus:border-[#03A8FD] focus:shadow-[#03A8FD]'/>
-                    <button className="absolute right-2 text-3xl"><IoIosCloseCircleOutline /></button>
-                </div>
-                <button className='bg-[#03A8FD] p-1 text-lg right-0 rounded-lg flex items-center pl-4 pr-4 text-white'>
-                    Add New User 
-                    <div className="text-2xl"><CiSquarePlus /> </div>
-                </button>
+    const [sidebar, setSidebar] = useState(false);
+
+    return (
+        <div className="flex w-full overflow-x-hidden">
+    <Sidebar showProfile={sidebar} />
+    <div className={`flex-grow rounded-lg mb-8 duration-300 ${sidebar ? "lg:w-[calc(100%-320px)] lg:ml-[320px]" : "lg:w-full lg:ml-0"} bg-[url('/src/assets/image2.svg')]`}>
+        <button className='bg-slate-200 p-2 rounded' onClick={() => { setSidebar(!sidebar) }}>
+            <Menu />
+        </button>
+
+        <div className="flex justify-between items-center mt-4">
+            <div className="w-[83%] relative flex items-center">
+                <button className="absolute text-3xl left-2"><CiSearch /></button>
+                <input className='w-full rounded-lg p-2 pl-12 focus:border-[#03A8FD] focus:shadow-[#03A8FD]' />
+                <button className="absolute right-2 text-3xl"><IoIosCloseCircleOutline /></button>
             </div>
-            <div className="bg-white grid p-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mt-8 rounded-lg ">
-                {/* No Moderators Found */}
-                <Cards role={'moderator'}/>
-            </div>
+            <button className='bg-[#03A8FD] p-1 text-lg right-0 rounded-lg flex items-center pl-4 pr-4 text-white'>
+                Add New User 
+                <div className="text-2xl"><CiSquarePlus /></div>
+            </button>
+        </div>
+
+        <div className="bg-white p-4 flex flex-wrap w-full mt-8 rounded-lg">
+            <Cards role={'moderator'} />
         </div>
     </div>
-  )
+</div>
+
+    );
 }
 
-export default AddUserComp
+export default AddUserComp;
+
 
 
 const data=[
@@ -94,14 +96,34 @@ const data=[
     }
 ]
 
-const Cards=({role})=>{
-    return(
+const Cards = ({ role }) => {
+    const currentDiv = useRef([]);
+
+    useEffect(() => {
+        gsap.fromTo(currentDiv.current, {
+            opacity: 0, 
+            y: 50,    
+            scaleX: 0,    
+        }, {
+            opacity: 1,   
+            y: 0,        
+            stagger: 0.2,
+            duration: 2,
+            scaleX: 1,
+            ease: 'elastic',
+        });
+    }, []);
+
+    return (
         <>
-            {
-                data.map((element,index)=>(
-                    <MemberCard key={index} role={role} data={element} />
-                ))
-            }
+            {data.map((element, index) => (
+                <div key={index}
+                    ref={ele => currentDiv.current[index] = ele}
+                    className="w-[calc(100%/4)]"
+                >
+                    <MemberCard role={role} data={element} />
+                </div>
+            ))}
         </>
-    )
-}
+    );
+};

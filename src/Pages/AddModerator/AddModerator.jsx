08cf589c72d302f5db99b7d96608b694373ca799
Soham Dutta <duttasoham2222@gmail.com>
2024-10-24 12:59:04 +0530

    seventh commit

---
+++ src/Pages/AddModerator/AddModerator.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import AddModeratorComp from '../../Components/AddModerator/AddModeratorComp'
+
+export default function AddModerator() {
+  return (
+    <div className='bg-[#ECECEC] p-8'><AddModeratorComp /></div>
+  )
+}

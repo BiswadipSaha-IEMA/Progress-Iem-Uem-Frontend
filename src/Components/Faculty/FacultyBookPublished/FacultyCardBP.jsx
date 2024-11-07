8a083e60b97880357009ee782622f76ea0cd32fc
somu545 <somuroy262@gmail.com>
2024-11-07 17:49:20 +0530

    updated

--- src/Components/Faculty/FacultyBookPublished/FacultyCardBP.jsx
+++ src/Components/Faculty/FacultyBookPublished/FacultyCardBP.jsx
@@ -151,6 +151,7 @@ function FacultyCardBP({ id, status, title, date, name, ISBN, driveFileUrl }) {
                                 onError={() => setIframeError(true)}
                             ></iframe>
                             {iframeError && (
+                                // if no data exist
                                 <div className="w-full h-full border border-dashed border-gray-300 rounded-lg flex items-center justify-center text-gray-400">
                                     <p className="text-center">Preview not available</p>
                                 </div>

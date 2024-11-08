0646f03d8ef8f35fc9462c28f57761abfeb6c9cf
BiswadipSaha-IEMA <biswadip.saha@iem.edu.in>
2024-11-08 17:21:13 +0530

    update ui

--- src/Pages/FacultyList/FacultyViewData/FacultyViewData.jsx
+++ src/Pages/FacultyList/FacultyViewData/FacultyViewData.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
-import ViewDataTable from "../../../Components/ViewData/ViewDataTable"
+import React from 'react';
+import ViewDataTable from "../../../Components/ViewData/ViewDataTable";
+import { dummyData as originalDummyData } from '../../../constants/studentData';
+import Header from '../../../Components/Header/Header';
 
 const FacultyViewData = () => {
+  // Map over the imported data to change the field name from "name" to "userName"
+  const modifiedData = originalDummyData.map(({ name, ...rest }) => ({
+    ...rest,
+    userName: name, // Replace "name" with "userName"
+  }));
+
   return (
     <>
-    <ViewDataTable/>
+      <Header backPage="" />
+      <ViewDataTable dummyData={modifiedData} name={'Book Published'} />
+      <ViewDataTable dummyData={modifiedData} name={'Book Published'} />
+      <ViewDataTable dummyData={modifiedData} name={'Book Published'} />
+      <ViewDataTable dummyData={modifiedData} name={'Book Published'} />
+      <ViewDataTable dummyData={modifiedData} name={'Book Published'} />
+      <ViewDataTable dummyData={modifiedData} name={'Book Published'} />
+      <ViewDataTable dummyData={modifiedData} name={'Book Published'} />
+      <ViewDataTable dummyData={modifiedData} name={'Book Published'} />
+      <ViewDataTable dummyData={modifiedData} name={'Book Published'} />
     </>
-  )
-}
+  );
+};
 
-export default FacultyViewData
+export default FacultyViewData;
--- src/constants/studentData.jsx
+++ src/constants/studentData.jsx
@@ -14,3 +14,12 @@
 // ];
 
 
+export const dummyData = [
+    { name: "John Doe", bookName: "React Basics", isbn: "123-4567890123", publisher: "Tech Books", date: "2022-01-15", submittedForms: "Yes" },
+    { name: "Jane Smith", bookName: "Advanced CSS", isbn: "987-6543210987", publisher: "Design Press", date: "2021-08-30", submittedForms: "No" },
+    { name: "Alice Johnson", bookName: "JavaScript Essentials", isbn: "321-6549871234", publisher: "Coding World", date: "2020-05-22", submittedForms: "Yes" },
+    { name: "Bob Lee", bookName: "UI/UX Principles", isbn: "654-3219876543", publisher: "Art Books", date: "2019-11-12", submittedForms: "Yes" },
+    { name: "Chris Martin", bookName: "Node.js Guide", isbn: "432-1098765432", publisher: "Tech Guides", date: "2023-03-01", submittedForms: "No" },
+    { name: "Emma Wilson", bookName: "Data Science 101", isbn: "567-8901234567", publisher: "Data Books", date: "2022-07-18", submittedForms: "Yes" },
+    { name: "James Brown", bookName: "Machine Learning Basics", isbn: "876-5432109876", publisher: "AI Press", date: "2021-02-25", submittedForms: "No" },
+  ];
